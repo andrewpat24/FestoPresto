d46b5c894893904a37a6b09358e4980184e22ef0
Andrew Patterson <apat24@mail.sfsu.edu>
2020-01-05 00:45:43 -0800

    Adds onChange functionality to input fields

--- frontend/components/EventForm.js
+++ frontend/components/EventForm.js
@@ -4,49 +4,78 @@ import { connect } from "react-redux";
 class EventForm extends React.Component {
   constructor(props) {
     super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+
     this.state = {
-      eventName: "New Event"
+      eventName: "New Event",
+      description: "",
+      location: "",
+      searchValue: ""
     };
   }
+
+  onFormSubmit = e => {};
+
+  onNameChange = e => {
+    this.setState({
+      eventName: e.target.value
+    });
+  };
+
+  onDescriptionChange = e => {
+    this.setState({
+      description: e.target.value
+    });
+  };
+
+  onLocationChange = e => {
+    this.setState({
+      location: e.target.value
+    });
+  };
 
   render() {
     return (
       <section component="EventForm">
         <div className="event-form-container">
           <div className="event-form">
-            <form>
-              <fieldset className="uk-fieldset">
-                <legend className="uk-legend">Legend</legend>
+            <form onSubmit={this.onFormSubmit}>
+              <fieldset className="uk-fieldset uk-form-width-large">
+                {/** TODO: Change the legend title to whatever's written in the name once there's input. If it's empty, change it back to New Event.*/}
+                <legend className="uk-legend">New Event</legend>
 
                 {/** Name */}
                 <div className="uk-margin">
                   <input
-                    className="uk-input uk-form-width-large uk-form-large"
+                    className="uk-input  uk-form-large"
                     type="text"
                     placeholder="Name"
+                    onChange={this.onNameChange}
                   />
                 </div>
 
                 {/** Description */}
-                <div class="uk-margin">
+                <div className="uk-margin">
                   <textarea
-                    class="uk-textarea"
+                    className="uk-textarea"
                     rows="5"
                     placeholder="Description.."
+                    onChange={this.onDescriptionChange}
                   ></textarea>
                 </div>
 
                 {/** Location */}
                 <div className="uk-margin">
                   <input
-                    className="uk-input uk-form-width-large"
+                    className="uk-input "
                     type="text"
                     placeholder="Location"
+                    onChange={this.onLocationChange}
                   />
                 </div>
                 {/** Event Dates */}
 
-                {/** Links */}
+                {/** Stages */}
 
                 {/** Links */}
               </fieldset>
