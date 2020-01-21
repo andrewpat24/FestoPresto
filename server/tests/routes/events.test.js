ea00874e8450b52ac107d27fd99801bd0f88494c
Andrew Patterson <apat24@mail.sfsu.edu>
2020-01-20 19:25:02 -0800

    [TESTS] Creates eventData file for use across tests. [TESTS] Creates tests for create_event route. [TESTS] Creates tests for /my_events route.

--- server/tests/routes/events.test.js
+++ server/tests/routes/events.test.js
@@ -4,15 +4,13 @@ import server from "../../server";
 // Bug is caused by lazy loading on jests' part.
 // https://stackoverflow.com/questions/49141927/express-body-parser-utf-8-error-in-test?rq=1
 import "../../../node_modules/iconv-lite/encodings";
+// TEST DATA
+import eventData from "../data/eventData";
+const testUserID = "testUserID";
+const testEventName = "testName";
 
-// test("Empty route should respond with 200.", () => {
-//   return request(server)
-//     .get("events/")
-//     .expect(200)
-//     .end(function(err, res) {
-//       if (err) throw err;
-//     });
-// });
+eventData.creator_uid = testUserID;
+eventData.name = testEventName;
 
 describe("Should test / Route", () => {
   it.only("should GET JSON and 200 response.", done => {
@@ -60,4 +58,67 @@ describe("Should test /get_event_by_id Route", () => {
         done();
       });
   });
+});
+
+describe("Should test /create_event Route", () => {
+  it.only("Should POST new event object data and recieve status 200, creator_uid, and name.", done => {
+    return request(server)
+      .post("/events/create_event")
+      .send(eventData)
+      .set("Accept", "application/json")
+      .expect(201)
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.body.creator_uid).toEqual(testUserID);
+        done();
+      });
+  });
+
+  // TODO: #77 - Reimplement this after setting rules on event data in the model
+  //
+  // it.only("Should POST invalid new event data and recieve status 500", done => {
+  //   return request(server)
+  //     .post("/events/create_event")
+  //     .send({})
+  //     .set("Accept", "application/json")
+  //     .expect(500)
+  //     .end((err, res) => {
+  //       if (err) throw err;
+  //       done();
+  //     });
+  // });
+});
+
+describe("Should test /my_events route", () => {
+  it.only("Should POST user id and recieve status 200 and user's events as an array of objects", done => {
+    return request(server)
+      .post("/events/my_events")
+      .send({
+        spotify_uid: testUserID
+      })
+      .set("Accept", "application/json")
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        console.log(res.body);
+        done();
+      });
+  });
+
+  // TODO: #78
+
+  // it.only("Should POST incorrect user id and recieve 500", () => {
+  //   const wrongId = "Wrong ID";
+  //   return request(server)
+  //     .post("/events/my_events")
+  //     .send({
+  //       spotify_uid: wrongId
+  //     })
+  //     .set("Accept", "application/json")
+  //     .expect(500)
+  //     .end((err, res) => {
+  //       if (err) throw err;
+  //       done();
+  //     });
+  // });
 });
