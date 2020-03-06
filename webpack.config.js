64558c52d0eaf728b896c587147301b89ea77d20
Andrew Patterson <apat24@mail.sfsu.edu>
2020-03-05 19:05:10 -0800

    [WEBPACK] Updates proxy for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require("path");
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.env.NODE_ENV === "test") {
-  require("dotenv").config({ path: ".env.test" });
-} else if (process.env.NODE_ENV === "development") {
-  require("dotenv").config({ path: ".env.development" });
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
 }
 
 module.exports = env => {
-  const isProduction = env === "production";
-  const CSSExtract = new ExtractTextPlugin("styles.css");
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
   return {
-    entry: ["babel-polyfill", "./frontend/app.js"],
+    entry: ['babel-polyfill', './frontend/app.js'],
     output: {
-      path: path.join(__dirname, "public", "dist"),
-      filename: "bundle.js"
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
     },
     module: {
       rules: [
         {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           test: /\.js$/,
           exclude: /node_modules/
         },
@@ -31,13 +31,13 @@ module.exports = env => {
           use: CSSExtract.extract({
             use: [
               {
-                loader: "css-loader",
+                loader: 'css-loader',
                 options: {
                   sourceMap: true
                 }
               },
               {
-                loader: "sass-loader",
+                loader: 'sass-loader',
                 options: {
                   sourceMap: true
                 }
@@ -50,33 +50,39 @@ module.exports = env => {
     plugins: [
       CSSExtract,
       new webpack.DefinePlugin({
-        "process.env.SPOTIFY_CLIENT_ID": JSON.stringify(
+        'process.env.SPOTIFY_CLIENT_ID': JSON.stringify(
           process.env.SPOTIFY_CLIENT_ID
         ),
-        "process.env.SPOTIFY_CLIENT_SECRET": JSON.stringify(
+        'process.env.SPOTIFY_CLIENT_SECRET': JSON.stringify(
           process.env.SPOTIFY_CLIENT_SECRET
         ),
-        "process.env.SPOTIFY_REDIRECT_URI": JSON.stringify(
+        'process.env.SPOTIFY_REDIRECT_URI': JSON.stringify(
           process.env.SPOTIFY_REDIRECT_URI
         ),
-        "process.env.MONGO_URI": JSON.stringify(process.env.MONGO_URI),
-        "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
+        'process.env.MONGO_URI': JSON.stringify(process.env.MONGO_URI),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(
           process.env.FIREBASE_STORAGE_BUCKET
         )
       })
     ],
-    devtool: isProduction ? "source-map" : "inline-source-map",
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
-      contentBase: path.join(__dirname, "public"),
+      contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
-      publicPath: "/dist/",
+      publicPath: '/dist/',
       proxy: {
-        "/api": {
-          target: "http://localhost:3000",
-          pathRewrite: {
-            "^/api": ""
-          }
-        }
+        '/api': isProduction
+          ? {
+              target: 'http://localhost:3000',
+              pathRewrite: {
+                '^/api': ''
+              }
+            }
+          : {
+              pathRewrite: {
+                '^/api': ''
+              }
+            }
       }
     }
   };
