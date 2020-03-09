023252a612f4eb4027421dc2df01428e430632e6
Andrew Patterson <apat24@mail.sfsu.edu>
2020-03-09 12:40:19 -0700

    [SERVER] Attempt 6

--- server/server.js
+++ server/server.js
@@ -63,12 +63,12 @@ app.use('/api/spotify', spotifyRouter);
 app.use('/api/events', eventsRouter);
 
 // React routes
-// const publicPath = path.join(__dirname, '../public');
-// app.use(express.static(publicPath));
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend', '../public/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../frontend', '../public/index.html'));
+// });
 
 // Port
 const port = process.env.PORT || 3000;
