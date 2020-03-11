ff62c5cf0769916890fb99cb5e8b95be66379903
Andrew Patterson <apat24@mail.sfsu.edu>
2020-03-11 13:18:44 -0700

    [SERVER] attempt 7

--- server/server.js
+++ server/server.js
@@ -63,8 +63,14 @@ app.use('/api/spotify', spotifyRouter);
 app.use('/api/events', eventsRouter);
 
 // React routes
-const publicPath = path.join(__dirname, '../public');
-app.use(express.static(publicPath));
+
+// app.use(express.static(publicPath));
+
+if (process.env.NODE_ENV === 'production') {
+  const publicPath = path.join(__dirname, '../public');
+  app.use(express.static(publicPath));
+  app.use('*', express.static(publicPath));
+}
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, '../frontend', '../public/index.html'));
