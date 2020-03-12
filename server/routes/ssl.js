2c542f271170a3ebc902586489ecc21d5f0b2268
Andrew Patterson <apat24@mail.sfsu.edu>
2020-03-11 23:41:08 -0700

    [CERTS] Re-adds ssl route

--- server/routes/ssl.js
+++ server/routes/ssl.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get(process.env.SSL_PATH_ONE, function(req, res) {
+router.get(`/.well-known/acme-challenge/${process.env.SSL_PATH_ONE}`, function(
+  req,
+  res
+) {
   res.send(process.env.SSL_CERT_ONE);
 });
 
-router.get(process.env.SSL_PATH_TWO, function(req, res) {
+router.get(`/.well-known/acme-challenge/${process.env.SSL_PATH_TWO}`, function(
+  req,
+  res
+) {
   res.send(process.env.SSL_CERT_TWO);
 });
 
