ae228e5d521111f0535c6302d6078f7f136b2a99
Andrew Patterson <apat24@mail.sfsu.edu>
2020-03-11 17:04:05 -0700

    [PASSPORT] Removes try/catch around async function calls

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,9 +1,9 @@
 // Passport imports
-const passport = require("passport");
-const SpotifyStrategy = require("passport-spotify").Strategy;
+const passport = require('passport');
+const SpotifyStrategy = require('passport-spotify').Strategy;
 
 // Moment
-const moment = require("moment");
+const moment = require('moment');
 
 // .env imports
 const client_id = process.env.SPOTIFY_CLIENT_ID; // Your client id
@@ -11,30 +11,31 @@ const client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
 const redirect_uri = process.env.SPOTIFY_REDIRECT_URI; // Your redirect uri
 
 // Mongoose
-const mongoose = require("mongoose");
-const User = mongoose.model("users");
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
   done(null, { id: user.id, access_token: user.accessToken });
 });
 
 passport.deserializeUser((spotify_user, done) => {
-  try {
-    User.findOne({ spotify_uid: spotify_user.id }, (err, user) => {
-      if (user === null) {
-        return done(null, {
-          id: "0",
-          access_token: "0"
-        });
-      }
+  User.findOne({ spotify_uid: spotify_user.id }, (err, user) => {
+    if (err) {
+      console.log('err:', e);
+      throw new Error(err);
+    }
+
+    if (user === null) {
       return done(null, {
-        id: user.spotify_uid,
-        access_token: user.spotify_access_token
+        id: '0',
+        access_token: '0'
       });
+    }
+    return done(null, {
+      id: user.spotify_uid,
+      access_token: user.spotify_access_token
     });
-  } catch (e) {
-    console.log("err:", e);
-  }
+  });
 });
 
 passport.use(
@@ -45,7 +46,7 @@ passport.use(
       callbackURL: redirect_uri
     },
     (accessToken, refreshToken, expires_in, profile, done) => {
-      const accessTokenExpirationDate = moment().add(expires_in, "seconds");
+      const accessTokenExpirationDate = moment().add(expires_in, 'seconds');
       User.findOne({ spotify_uid: profile.id }, (err, user) => {
         if (!user) {
           const newUser = new User({
@@ -58,14 +59,13 @@ passport.use(
             photos: profile.photos
           });
 
-          try {
-            newUser.save(err => {
-              if (err) throw new Error(err);
-              return done(null, { ...profile, accessToken });
-            });
-          } catch (err) {
-            console.log("Error saving new user.. : ", err);
-          }
+          newUser.save(err => {
+            if (err) {
+              console.log('Error saving new user.. : ', err);
+              throw new Error(err);
+            }
+            return done(null, { ...profile, accessToken });
+          });
         } else {
           return done(null, { ...profile, accessToken });
         }
