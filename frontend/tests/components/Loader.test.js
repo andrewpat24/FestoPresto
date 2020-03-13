078031aabad4a69ceaf2f4f4ef30f369ee1fa1a7
Andrew Patterson <apat24@mail.sfsu.edu>
2020-03-13 13:45:33 -0700

    removes unused tests

--- frontend/tests/components/Loader.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import { Loading } from "../../components/Loading";
-
-test("Should render Loading component", () => {
-  const wrapper = shallow(<Loading />);
-  expect(wrapper).toMatchSnapshot();
-});
