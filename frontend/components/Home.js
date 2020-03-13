eeb861a2a48ee3cbc252c7a4d901e2c83a53eea2
Andrew Patterson <apat24@mail.sfsu.edu>
2020-03-13 16:57:11 -0700

    [COMPONENT] changes wording

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -5,8 +5,8 @@ export const Home = () => (
     <div className="home-header">
       <h1 className="uk-text-center uk-header-medium">Festivus Music</h1>
       <h1 className="uk-text-center uk-header-small">
-        Search for festivals by city, then filter their lineups by genre and
-        make some playlists!
+        Search for festivals by city, then filter their lineups by genre to make
+        some playlists!
       </h1>
     </div>
   </div>
