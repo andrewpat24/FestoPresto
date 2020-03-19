c46dbeabcc414e6cbee1f05ded1948ec98e0f138
Andrew Patterson <apat24@mail.sfsu.edu>
2020-03-18 19:33:31 -0700

    [COMPONENT] Replaces artist info on card with genre and follower count

--- frontend/components/ArtistCard.js
+++ frontend/components/ArtistCard.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
 export const ArtistCard = props => {
-  const { _id, artist_name, songkick_url, popularity, followers, img } = props;
-  console.log(img);
+  const { artist_name, songkick_url, followers, img, genres } = props;
+
   const artistImage = !!img ? img : '/images/uikit-test-img.jpg';
+  const genre = !!genres[0] ? genres[0] : 'No genre';
+  const readableFollowerCount = (followers => {
+    let response = '';
+
+    if (!followers) {
+      return 'No data';
+    }
+
+    if (followers > 1000000) {
+      response = `${Math.round(followers / 1000000)}m followers`;
+    } else if (followers > 1000) {
+      response = `${Math.round(followers / 1000)}k followers`;
+    } else {
+      response = `${followers} followers`;
+    }
+
+    return response;
+  })(followers);
+
+  if (!genres[0] && !followers) {
+  }
+
   return (
     <div>
-      <a href={songkick_url} target="_blank">
+      <a className="artist-card-link" href={songkick_url} target="_blank">
         <div className="uk-card uk-card-default artist-card">
           <div className="uk-card-media-top">
             <img className="artist-image" src={artistImage} />
@@ -15,11 +37,14 @@ export const ArtistCard = props => {
             <div className="card-title-area uk-flex-center uk-flex">
               <h3 className="uk-card-title artist-title">{artist_name}</h3>
             </div>
-            <div className="card-detail-area uk-flex uk-flex-center">
-              <div>Popularity: {popularity}</div>
-              <span className="spacer" />
-              <div>Followers: {followers}</div>
-            </div>
+            {!!genres[0] || !!followers ? (
+              <div className="card-detail-area uk-flex uk-flex-center">
+                <div className="col-left">{genre}</div>
+                <div className="col-right">{readableFollowerCount}</div>
+              </div>
+            ) : (
+              <div className="card-detail-area uk-flex uk-flex-center" />
+            )}
           </div>
         </div>
       </a>
